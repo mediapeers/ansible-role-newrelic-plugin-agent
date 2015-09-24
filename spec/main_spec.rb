4136e01609c29290193b7d2bcc7181a3895b9ecf
Stefan Horning <stefan@hornings.de>
2015-09-24 18:08:09 +0200

    Small fix in template, using correct yaml filter. Updated specs. Althouth the string from the specs somehwo doesn't exactly match yet

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe "Newrelic plugin agent setup" do
+  plugin_configs = ANSIBLE_VARS.fetch('newrelic_plugins', 'FAIL')
+
   describe package('python-pip') do
-    it { should be installed }
+    it { should be_installed }
   end
 
   describe package('python-dev') do
@@ -11,16 +13,19 @@ describe "Newrelic plugin agent setup" do
 
   describe file('/etc/newrelic/newrelic-plugin-agent.cfg') do
     it { should exist }
-    it { should contain('nginx') }
+    plugin_configs.each do |elem|
+      config_string = elem.to_yaml.delete("-").gsub("\n", "\n  ").strip
+      its(:content) { should include(config_string) }
+    end
   end
 
   describe file('/etc/init/newrelic-plugin-agent.conf') do
     it { should exist }
-    it { should contain('newrelic-plugin-agent -c /etc/newrelic/newrelic-plugin-agent.cfg') }
+    its(:content) { should include('newrelic-plugin-agent -c /etc/newrelic/newrelic-plugin-agent.cfg') }
   end
 
   describe service('newrelic-plugin-agent') do
     it { should be_enabled }
-    it { should be_running }
+    it { should be_running }.
   end
 end
